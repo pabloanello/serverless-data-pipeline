2ca228800fec26aef04c701538e145777dcfb5f7
Pablo Anello <pablo.anello@tarmac.io>
2025-08-24 20:49:14 -0300

    Add handler unit test cases

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,1 +1,12 @@
 # test_handler.py
+
+def test_example():
+    assert True
+
+def test_handler_exists():
+    try:
+        import src.handler
+        assert True
+    except ImportError:
+        assert False
+
